f57414fb90b250667bb0fa5d5b03d083b0bb01e5
mgw <martingerdin@gmail.com>
2022-11-07 22:58:29 +0100

    Start writing pilot publication analysis script

---
+++ pilot-publication/main.R
@@ -0,0 +1,14 @@
+## Load packages
+library(dotenv)
+
+## Import data
+data <- readr::read_csv(Sys.getenv("DATA_DIR"))
+
+## Import codebook
+codebook.arguments <- lapply(c("URL", "UID", "USERNAME", "PASSWORD"),
+                             function(x) Sys.getenv(paste0("KOBO_", x)))
+codebook <- do.call(noacsr::kobo_get_project_codebook, codebook.arguments)
+
+## Label variables
+
+
