3a53a0b138c45f8fea711fb9cf357241ab8602de
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2023-12-29 21:59:09 +0100

    Edit code to generate outcome tables

--- pilot-publication/functions/outcomes.R
+++ pilot-publication/functions/outcomes.R
@@ -59,7 +59,7 @@ binary_outcomes <- function() {
           )
      )
      binary.outcomes <- lapply(binary.outcomes, function(binary.outcome) {
-          binary.outcome$hchange.from.baseline <- TRUE
+          binary.outcome$change.from.baseline <- TRUE
           return(binary.outcome)
      })
      names(binary.outcomes) <- sapply(binary.outcomes, function(binary.outcome) binary.outcome$name)
@@ -67,29 +67,65 @@ binary_outcomes <- function() {
 }
 
 categorical_outcomes <- function() {
-     setNames(nm = c(
-          "outcomes__eq5dm",
-          "outcomes__eq5dsc",
-          "outcomes__eq5dua",
-          "outcomes__eq5dpd",
-          "outcomes__eq5dad",
-          "outcomes__eq5dm30d",
-          "outcomes__eq5dsc30d",
-          "outcomes__eq5dua30d",
-          "outcomes__eq5dpd30d",
-          "outcomes__eq5dad30d",
-          "outcomes__patient_satisfaction"
-     ))
+     categorical.outcomes <- list(
+          list(
+               name = "outcomes__eq5dm",
+               label = "EQ-5D mobility at discharge"),
+          list(name = "outcomes__eq5dsc",
+               label = "EQ-5D self-care at discharge"),
+          list(name = "outcomes__eq5dua",
+               label = "EQ-5D usual activities at discharge"),
+          list(name = "outcomes__eq5dpd", 
+               label = "EQ-5D pain/discomfort at discharge"),
+          list(name = "outcomes__eq5dad",
+               label = "EQ-5D anxiety/depression at discharge"),
+          list(name = "outcomes__eq5dm30d",
+               label = "EQ-5D mobility at 30 day follow-up"),
+          list(name = "outcomes__eq5dsc30d",
+               label = "EQ-5D self-care at 30 day follow-up"),
+          list(name = "outcomes__eq5dua30d",
+               label = "EQ-5D usual activities at 30 day follow-up"),
+          list(name = "outcomes__eq5dpd30d",
+               label = "EQ-5D pain/discomfort at 30 day follow-up"),
+          list(name = "outcomes__eq5dad30d",
+               label = "EQ-5D anxiety/depression at 30 day follow-up"),
+          list(name = "outcomes__patient_satisfaction",
+               label = "Patient satisfaction")
+     )
+     categorical.outcomes <- lapply(categorical.outcomes, function(categorical.outcome) {
+          categorical.outcome$change.from.baseline <- TRUE
+          return(categorical.outcome)
+     })
+     names(categorical.outcomes) <- sapply(categorical.outcomes, function(categorical.outcome) categorical.outcome$name)
+     return(categorical.outcomes)
 }
 
 ## Complete later
 quantitative_outcomes <- function() {
-     setNames(nm = c(
-          "complications__number_of_hospitalizations_for_this_injury",
-          "outcomes__eq5dhs",
-          "outcomes__eq5dhs30d",
-          "outcomes__cost_of_treatment"
-     ))
+     quantitative.outcomes <- list(
+          list(
+               name = "complications__number_of_hospitalizations_for_this_injury",
+               label = "Number of hospitalizations for this injury"
+          ),
+          list(
+               name = "outcomes__eq5dhs",
+               label = "EQ-5D health state at discharge"
+          ),
+          list(
+               name = "outcomes__eq5dhs30d",
+               label = "EQ-5D health state at 30 day follow-up"
+          ),
+          list(
+               name = "outcomes__cost_of_treatment",
+               label = "Cost of treatment"
+          )
+     )
+     quantitative.outcomes <- lapply(quantitative.outcomes, function(quantitative.outcome) {
+          quantitative.outcome$change.from.baseline <- TRUE
+          return(quantitative.outcome)
+     })
+     names(quantitative.outcomes) <- sapply(quantitative.outcomes, function(quantitative.outcome) quantitative.outcome$name)
+     return(quantitative.outcomes)
 }
 
 ## Complete later
@@ -98,4 +134,15 @@ time_to_event_outcomes <- function() {
           "outcomes__time_of_death",
           "interventions__date_of_surgery"
      ))
+}
+
+## Function to return all outcomes
+all_outcomes <- function() {
+     outcomes <- c(
+          binary_outcomes(),
+          categorical_outcomes(),
+          quantitative_outcomes(),
+          time_to_event_outcomes()
+     )
+     return(outcomes)
 }
