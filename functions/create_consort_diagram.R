1eb463827e4e44691f76dfd13b13f837ed031e3f
martingerdin <martingerdin@gmail.com>
2025-06-27 19:24:10 +0200

    Continue to work on response to reviewers

--- functions/create_consort_diagram.R
+++ functions/create_consort_diagram.R
@@ -11,7 +11,13 @@ create_consort_diagram <- function(results) {
     str_replace(coll("n.atls.residents"), n.atls.residents) |>
     str_replace(coll("n.atls"), n.atls) |>
     str_replace(coll("n.ptc.residents"), n.ptc.residents) |>
-    str_replace(coll("n.ptc"), n.ptc)
+    str_replace(coll("n.ptc"), n.ptc) |>
+    str_replace(coll("n.control.complete.m30d"), n.control.complete.m30d) |>
+    str_replace(coll("n.control.complete.hd"), n.control.complete.hd) |>
+    str_replace(coll("n.atls.complete.m30d"), n.control.complete.m30d) |>
+    str_replace(coll("n.atls.complete.hd"), n.atls.complete.hd) |>
+    str_replace(coll("n.ptc.complete.m30d"), n.atls.complete.m30d) |>
+    str_replace(coll("n.ptc.complete.hd"), n.ptc.complete.hd)
   writeLines(consort.svg, "modified-consort-flow-diagram.svg")
   system("svgexport modified-consort-flow-diagram.svg consort-flow-diagram.png 2x")
 }
